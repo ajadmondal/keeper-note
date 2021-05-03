1324b73102b123a10aed743f1eb025179aaab102
Sk Ajad Mondal <53702822+ajadmondal@users.noreply.github.com>
2021-05-03 23:44:55 +0530

    made the header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+
+    </div>
+    )
+    ;
+}
+
+export default Header;
