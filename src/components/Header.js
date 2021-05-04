276c1f810022e43b8186d75f665f6ef78faa94ff
Sk Ajad Mondal <53702822+ajadmondal@users.noreply.github.com>
2021-05-04 09:51:18 +0530

    header bar is made

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './styles/Header.css';
+import { SiGreenkeeper } from "react-icons/si";
 
 function Header() {
     return (
-        <div className="header">
-
-    </div>
-    )
-    ;
+      <div className="header">
+        <div className="header__content">
+          <h2 className="brand">
+            <SiGreenkeeper className="brand__icon"/>
+            Keeper
+          </h2>
+          <div className="user">
+            <h4 className="user__name">Hello Guest</h4>
+            <h3 className="log__info">Log In</h3>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Header;
