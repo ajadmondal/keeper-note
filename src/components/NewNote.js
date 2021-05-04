ee7ee0f086d3d55f11959dbf64b529ebf0db072f
Sk Ajad Mondal <53702822+ajadmondal@users.noreply.github.com>
2021-05-04 15:57:42 +0530

    completed the new note form

---
+++ src/components/NewNote.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import './styles/NewNote.css';
+import { RiAddCircleFill } from "react-icons/ri";
+
+function NewNote() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title, description);
+    setTitle("");
+    setDescription("");
+  }
+    return (
+      <form className="new-note" onSubmit={addNewPost}>
+        <input
+          className="new-note__title"
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          placeholder="Title"
+        />
+        <textarea
+          className="new-note__description"
+          name="Description"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          placeholder="Description"
+          // cols="55"
+          // rows="4"
+          style={{ resize: "none" }}
+        ></textarea>
+        <button type="submit" className="add-btn"><RiAddCircleFill/></button>
+      </form>
+    );
+}
+
+export default NewNote
