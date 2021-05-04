611a98b97282fc20fd0c29fc0dbd7887002527cd
Sk Ajad Mondal <53702822+ajadmondal@users.noreply.github.com>
2021-05-04 17:47:07 +0530

    made the note card

--- src/App.js
+++ src/App.js
@@ -18,6 +18,54 @@ function App() {
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s,"
           />
+          <Note
+            title="what to do"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,"
+          />
+          <Note
+            title="what to do"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,"
+          />
+          <Note
+            title="what to do"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,"
+          />
+          <Note
+            title="what to do"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,"
+          />
+          <Note
+            title="what to do"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,"
+          />
+          <Note
+            title="what to do"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,"
+          />
+          <Note
+            title="what to do"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,"
+          />
+          <Note
+            title="what to do"
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,"
+          />
         </div>
       </div>
     </div>
