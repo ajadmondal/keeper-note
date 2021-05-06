513b217a2625fe461f6f5f3f2ac156a91b2ce96e
Sk Ajad Mondal <53702822+ajadmondal@users.noreply.github.com>
2021-05-06 15:45:54 +0530

    upadating to database functionality is added to newNote

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,40 +1,45 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './styles/NewNote.css';
+import { v4 as uuid } from "uuid";
 import { RiAddCircleFill } from "react-icons/ri";
 
-function NewNote() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+function NewNote({title, description, setTitle, setDescription, updateNoteToDB }) {
+  
+  
   const addNewPost = (e) => {
     e.preventDefault();
-    console.log(title, description);
-    setTitle("");
-    setDescription("");
-  }
-    return (
-      <form className="new-note" onSubmit={addNewPost}>
-        <input
-          className="new-note__title"
-          type="text"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-          placeholder="Title"
-        />
-        <textarea
-          className="new-note__description"
-          name="Description"
-          value={description}
-          onChange={(e) => {
-            setDescription(e.target.value);
-          }}
-          placeholder="Description"
-          style={{ resize: "none" }}
-        ></textarea>
-        <button type="submit" className="add-btn"><RiAddCircleFill/></button>
-      </form>
-    );
+    const noteId = uuid();
+    updateNoteToDB(noteId, title, description);
+    // console.log(noteId, title, description);
+    // setTitle("");
+    // setDescription("");
+  };
+  return (
+    <form className="new-note" onSubmit={addNewPost}>
+      <input
+        className="new-note__title"
+        type="text"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        placeholder="Title"
+      />
+      <textarea
+        className="new-note__description"
+        name="Description"
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+        placeholder="Description"
+        style={{ resize: "none" }}
+      ></textarea>
+      <button type="submit" className="add-btn">
+        <RiAddCircleFill />
+      </button>
+    </form>
+  );
 }
 
 export default NewNote
