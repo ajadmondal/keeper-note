6a92eab6b2064096f1aac178f53a25c9099897f8
Sk Ajad Mondal <53702822+ajadmondal@users.noreply.github.com>
2021-05-08 16:16:19 +0530

    added the close function to close the zppmModal

--- src/components/ZoomModal.js
+++ src/components/ZoomModal.js
@@ -4,24 +4,23 @@ import { ImCross } from "react-icons/im";
 import './styles/ZoomModal.css';
 
 function ZoomModal(props) {
+    const close = () => {
+      props.setZoom(!props.zoom);
+    };
     if (!props.zoom) return null;
 
     return ReactDOM.createPortal(
       <div className="container">
         <div
           className="dropback"
-          onClick={() => {
-            props.setZoom(!props.zoom);
-          }}
+          onClick={close}
         ></div>
         <div className="content">
           <h3 className="note-title">{props.title}</h3>
           <p className="note-description">{props.description}</p>
           <ImCross
             className="remove"
-            onClick={() => {
-              props.setZoom(!props.zoom);
-            }}
+            onClick={close}
           />
         </div>
       </div>,
