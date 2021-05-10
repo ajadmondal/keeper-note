88d50ec1812e30a74ca706ddf6c4769deaae9b47
Sk Ajad Mondal <53702822+ajadmondal@users.noreply.github.com>
2021-05-10 23:58:38 +0530

    fixed update issue in editModal component

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -6,24 +6,16 @@ import "./styles/ZoomModal.css";
 import "./styles/NewNote.css";
 
 function EditModal(props) {
-  const title = props.title;
-    const description = props.description;
-  const close = () => {
-    props.updateNoteToDB(props.noteId, title, description);
-    props.setTitle(title);
-    props.setDescription(description);
-    props.setEdit(!props.edit);
-  };
   if (!props.edit) return null;
 
   return ReactDOM.createPortal(
     <div className="container">
-      <div className="dropback" onClick={close}></div>
+      <div className="dropback" onClick={props.editNote}></div>
       <div className="content">
         <input
           className="new-note__title note-title"
           type="text"
-          value={title}
+          value={props.title}
           onChange={(e) => {
             props.setTitle(e.target.value);
           }}
@@ -32,15 +24,15 @@ function EditModal(props) {
         <textarea
           className="note-description new-note__description"
           name="Description"
-          value={description}
+          value={props.description}
           onChange={(e) => {
             props.setDescription(e.target.value);
           }}
           placeholder="Description"
           style={{ resize: "none" }}
         ></textarea>
-        <ImCross className="remove" onClick={close} />
-        <MdLibraryAdd className="save" onClick={close} />
+        <ImCross className="remove" onClick={props.editNote} />
+        <MdLibraryAdd className="save" onClick={props.editNote} />
       </div>
     </div>,
     document.getElementById("portal")
