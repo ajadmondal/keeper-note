17d03a4520736b8440cae75e5c21dcbd70640629
Sk Ajad Mondal <53702822+ajadmondal@users.noreply.github.com>
2021-05-13 08:49:50 +0530

    Notifier is editted to manage different evnets

--- src/components/Notifier.js
+++ src/components/Notifier.js
@@ -1,17 +1,33 @@
-import React, {useEffect} from 'react'
-import ReactDOM from 'react-dom';
+import React, { useEffect } from 'react';
+import { FaRegCheckCircle } from "react-icons/fa";
+import { MdErrorOutline } from "react-icons/md";
+
 import './styles/Notifier.css';
 
 function Notifier(props) {
     useEffect(() => {
-        const id = setTimeout(() => props.setShowNotifier(0), 3000);
+      const id = setTimeout(() => {
+        props.setNotifierComment("");
+        props.setShowNotifier(0);
+      }, 2500);
         return (() => clearTimeout(id));
     }, [])
     // if (props.showNotifier % 2 !== 0) return null;
     // else
       return (
-        <div className="notifier">
-          <p>Document successfully Updated</p>
+        <div
+          className={`notifier ${
+            props.showNotifier === 1 ? "success" : "error"
+          }`}
+        >
+          <div className="notifier__icon">
+            {props.showNotifier === 1 ? (
+              <FaRegCheckCircle />
+            ) : (
+              <MdErrorOutline />
+            )}
+          </div>
+          <p>{props.notifierComment}</p>
         </div>
       );
 }
